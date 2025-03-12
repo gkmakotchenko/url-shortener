187a30f7b17431ccd5a940b43dcaf66deb2f9100
Gleb Makotchenko <glebmakotchenko@MacBook-Air-Gleb.local>
2025-03-12 14:09:53 +0500

    First commit

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"Users/glebmakotchenko/Documents/vscode/url-shortener/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	os.Setenv("CONFIG_PATH", "../../config/local.yaml")
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+	log.Info("test")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
